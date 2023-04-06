7dc06f580525286722e7124937e518642ae5024e
DreamWake <2509415676@qq.com>
2023-04-06 15:23:33 +0800

    Hello Git

---
+++ HelloGit/src/main/java/com/ws/HelloGit.java
@@ -0,0 +1,14 @@
+package com.ws;
+
+/**
+ * @program: GitRepository
+ * @description:
+ * @author: WS
+ * @create: 2023-04-06 15:20
+ **/
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
